7698fcf130fa6939864c086f23fdd27336039486
jason0320 <jasonnosaj0320@gmail.com>
2024-12-13 23:47:37 +0800

    加入專案檔案。

---
+++ FeatBookPatch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace PandaFeatBookMod
+{
+    [HarmonyPatch]
+    internal class FeatBookPatch
+    {
+        [HarmonyTranspiler]
+        [HarmonyPatch(typeof(TraitBookSecret), "OnRead")]
+        static void Postfix(Chara c)
+        {
+            if (c.IsPC)
+            {
+                EClass.player.stats.kumi--;
+            }
+        }
+    }
+}
---
+++ Mod_PandaFeatBookMod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using HarmonyLib;
+
+namespace PandaFeatBookMod
+{
+    [BepInPlugin("panda.featbook.mod", "Panda's Feat Book Mod", "1.0.0.0")]
+    internal class Mod_PandaFeatBookMod : BaseUnityPlugin
+    {
+        private void Start()
+        {
+            var harmony = new Harmony("Panda's Feat Book Mod");
+            harmony.PatchAll();
+        }
+    }
+}
